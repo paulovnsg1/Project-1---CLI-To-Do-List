919656649a64aaaa2369ab0465634cb4a1eaaf2d
paulovnsg1 <paulovictor.gomes004@email.com>
2026-08-27 16:29:05 -0700

    Initial project setup with CMake

---
+++ build/CMakeFiles/todo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todo.dir/src/main.cpp.obj"
+  "CMakeFiles/todo.dir/src/main.cpp.obj.d"
+  "libtodo.dll.a"
+  "todo.exe"
+  "todo.exe.manifest"
+  "todo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Tudo Funcionando";
+    return 0;
+}
