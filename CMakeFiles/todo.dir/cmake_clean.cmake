5aa9c7b2e69bdc5672ffcf3552a61d2e4b4af11d
paulovnsg1 <paulovictor.gomes004@email.com>
2026-08-29 14:42:19 -0700

    Add Task class

---
+++ CMakeFiles/todo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todo.dir/src/Task.cpp.obj"
+  "CMakeFiles/todo.dir/src/Task.cpp.obj.d"
+  "CMakeFiles/todo.dir/src/main.cpp.obj"
+  "CMakeFiles/todo.dir/src/main.cpp.obj.d"
+  "libtodo.dll.a"
+  "todo.exe"
+  "todo.exe.manifest"
+  "todo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
